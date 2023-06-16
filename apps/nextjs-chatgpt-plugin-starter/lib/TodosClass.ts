52b92f60fb7faa8ef1505c2d1484d4878cf3fe7e
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-06-16 11:25:00 +0300

    Small improvement.

--- apps/nextjs-chatgpt-plugin-starter/lib/TodosClass.ts
+++ apps/nextjs-chatgpt-plugin-starter/lib/TodosClass.ts
@@ -6,15 +6,15 @@ type TodoList = components["schemas"]["TodoList"];
 class TodosClass {
   todos: TodoList["todos"] = [];
   createTodo = (todo: Todo) => {
-    this.todos = [...this?.todos, todo];
+    this.todos = [...(this?.todos as any), todo];
   };
   getTodos(): TodoList {
     const todos = this.todos;
     return { todos };
   }
   deleteTodo(todo: Todo): void {
-    const index = this?.todos?.findIndex((x) => x?.todo === x?.todo);
-    this.todos.splice(index, 1);
+    const index = this?.todos?.findIndex((x: any) => x?.todo === x?.todo);
+    this?.todos?.splice(index as any, 1);
   }
 }
 
