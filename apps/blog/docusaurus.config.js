37fce5777a679ca7408931fc1f7370d21e1ca256
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-11-05 22:29:31 +0200

    Small improvement.

--- apps/blog/docusaurus.config.js
+++ apps/blog/docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/nicolad/openai-kitchen-sink/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/nicolad/openai-kitchen-sink/tree/main/apps/blog/templates/shared/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
